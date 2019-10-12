f5fc0f52080b7ed55fb4ac81103e0cc70347893e
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-10-12 13:38:50 -0300

    Routes configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div>
-      <h1>oi</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
